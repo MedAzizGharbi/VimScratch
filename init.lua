838af7ef753a56a6932d94a8c62d46e0507ca8ee
MedAzizGharbi <gharbi.aziz03@gmail.com>
2024-12-20 04:18:31 +0100

    First Commit

---
+++ init.lua
@@ -0,0 +1,16 @@
+print("Config hboleya")
+require("config.lazy")
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n" , "<space><space>x", "<cmd>source %<CR>")
+
+vim.api.nvim_create_autocmd('TextYankPost',{
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('highlight-yank', {clear = true}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
