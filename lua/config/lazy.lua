7d8969389782f0ea9c406096ddd3939fc4c4d080
MedAzizGharbi <gharbi.aziz03@gmail.com>
2024-12-20 16:28:12 +0100

    Telescope LSP and other cool stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,9 +20,14 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-	spec = {
-		{ "rose-pine/neovim", name = "rose-pine", config = function() vim.cmd("colorscheme rose-pine") end },
-		-- import your plugins
-		{ import = "plugins" },
-	},
+  spec = {
+    { "rose-pine/neovim", name = "rose-pine", config = function() vim.cmd("colorscheme rose-pine") end },
+    -- import your plugins
+    { import = "config.plugins" },
+  },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = false,
+    notify = false, -- get a notification when changes are found
+  },
 })
