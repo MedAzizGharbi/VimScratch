bc3bd0e9202f99e83b0c72642f4bef6f866ce87f
MedAzizGharbi <gharbi.aziz03@gmail.com>
2024-12-22 03:47:56 +0100

    Chwaya fazet mel kickstart
    UI Select using telescope for mason and lsp improvement also fidget.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,30 +1,34 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      { "nvim-telescope/telescope-ui-select.nvim" },
     },
     config = function()
-      require('telescope').setup {
+      require("telescope").setup({
         pickers = {
           find_files = {
-            theme = "ivy"
-          }
+            theme = "ivy",
+          },
         },
         extensions = {
-          fzf = {}
-        }
-      }
-
-      require('telescope').load_extension('fzf')
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      require 'config.telescope.multigrep'.setup()
-    end
-  }
+          fzf = {},
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+      require("telescope").load_extension("fzf")
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      require("config.telescope.multigrep").setup()
+    end,
+  },
 }
